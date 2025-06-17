0a8ef49bbe5d6e842d1b022a0341f4cf4fbfe831
shivam <rajshivam0432@gmail.com>
2025-06-17 16:52:59 +0530

    fixes vercel.json

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,7 @@ app.use(
   cors({
     origin: "https://bytebattle-platform.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
